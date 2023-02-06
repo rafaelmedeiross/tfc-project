4094c09df14af35d1674471000a737daa97ca6fd
rafaelmedeiross <rafael.medeiros@msn.com>
2023-02-06 16:33:23 -0300

    feat: added matches searches

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -5,6 +5,11 @@ class MatchController {
   constructor(private matchService = new MatchService()) { }
 
   public getAllMatches = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    if (inProgress !== undefined) {
+      const feedback = await this.matchService.getAllMatches(inProgress as string);
+      return res.status(200).json(feedback);
+    }
     const feedback = await this.matchService.getAllMatches();
     res.status(200).json(feedback);
   };
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -5,8 +5,11 @@ import Team from '../database/models/Team';
 class MatchService {
   constructor(private matchModel = Match) {}
 
-  public async getAllMatches(): Promise<IMatch[]> {
+  public async getAllMatches(inProgress?: string): Promise<IMatch[]> {
+    const inProgressStatus = inProgress === 'true';
+    const where = inProgress ? { inProgress: inProgressStatus } : {};
     const allMatches = await this.matchModel.findAll({
+      where,
       include: [
         { model: Team, as: 'homeTeam', attributes: ['teamName'] },
         { model: Team, as: 'awayTeam', attributes: ['teamName'] },
